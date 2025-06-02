7ef1e54e8d6a23f5e9a1bda71a3778018dda553c
Deiv <32deiv@gmail.com>
2025-06-02 16:59:23 -0300

    fortified backend encapsulation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,5 @@
 module props_class_generator {
-	requires org.checkerframework.checker.qual;
-	requires org.junit.jupiter.api;
+	requires static org.junit.jupiter.api;
 	
 	exports com.dsl.classgen;
-	exports com.dsl.classgen.annotations;
-	exports com.dsl.classgen.parsers;
-	exports com.dsl.classgen.annotations.containers;
-	exports com.dsl.classgen.services;
 }
