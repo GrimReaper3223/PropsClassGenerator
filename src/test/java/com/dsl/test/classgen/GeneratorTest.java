39fae02ec1988e39aa3d389a58f07dc3fa82d030
Deiv <32deiv@gmail.com>
2025-06-06 15:47:48 -0300

    improved test

--- src/test/java/com/dsl/test/classgen/GeneratorTest.java
+++ src/test/java/com/dsl/test/classgen/GeneratorTest.java
@@ -1,27 +1,41 @@
 package com.dsl.test.classgen;
 
 import java.nio.file.Path;
+import java.util.concurrent.ExecutionException;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import com.dsl.classgen.Generator;
+import com.dsl.classgen.io.GeneratedStructureChecker;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class GeneratorTest {
 
 	static Path inPropsPath = Path.of("/home/Deiv/workspace/git/SellerTree-Commercial/AdminManager/src/main/resources/values/strings/");
 	static String packageClass = "com.dsl.classgen";
 	
 	@BeforeAll
-//	@Disabled("Teste bem-sucedido")
 	static void initTest() {
 		Generator.init(inPropsPath, packageClass, true);
 	}
 	
 	@Test
-//	@Disabled("Teste bem-sucedido")
+	@Order(1)
+	@Disabled("Teste bem-sucedido")
 	void generateTest() {
 		Generator.generate();
+	}
+	
+	@Test
+	@Order(2)
+	@Disabled("Teste bem-sucedido")
+	void isExistsPackage() throws InterruptedException, ExecutionException {
+		Assertions.assertTrue(GeneratedStructureChecker.checkGeneratedStructure());
 	}
 }
