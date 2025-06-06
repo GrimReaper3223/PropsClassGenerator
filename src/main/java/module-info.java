216c70eae3c600b4d98e79a135dc556cde186f55
Deiv <32deiv@gmail.com>
2025-06-06 15:48:28 -0300

    added gson dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module props_class_generator {
 	requires static org.junit.jupiter.api;
+	requires com.google.gson;
 	
 	exports com.dsl.classgen;
 	exports com.dsl.classgen.annotations;
+	
+	opens com.dsl.classgen.io to com.google.gson;
 }
