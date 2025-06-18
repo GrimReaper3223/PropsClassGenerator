0e6e08915bd22466444185218b6886b41b63398a
Deiv <32deiv@gmail.com>
2025-06-18 17:13:29 -0300

    fixed module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module props_class_generator {
 	requires static org.junit.jupiter.api;
 	requires com.google.gson;
+	requires java.compiler;
 	
 	exports com.dsl.classgen;
 	exports com.dsl.classgen.annotations;
+	exports com.dsl.classgen.annotations.containers;
 	
 	opens com.dsl.classgen.io to com.google.gson;
 }
