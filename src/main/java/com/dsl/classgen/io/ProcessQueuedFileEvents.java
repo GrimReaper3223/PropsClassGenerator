a4829017deba7b96dec46932a2fa17d374d97471
Deiv <32deiv@gmail.com>
2025-06-28 17:42:12 -0300

    Added some progress on processing file changes

--- src/main/java/com/dsl/classgen/io/ProcessQueuedFileEvents.java
+++ src/main/java/com/dsl/classgen/io/ProcessQueuedFileEvents.java
@@ -1,5 +1,6 @@
 package com.dsl.classgen.io;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.WatchEvent;
@@ -7,6 +8,8 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.dsl.classgen.io.cache_system.FileCacheSystem;
+import com.dsl.classgen.io.file_handler.SyncSource;
 import com.dsl.classgen.services.WatchServiceImpl;
 import com.dsl.classgen.utils.Utils;
 
@@ -48,6 +51,7 @@ public class ProcessQueuedFileEvents {
 		eventProcessorThread.interrupt();
 	}
 	
+	// adiciona um novo arquivo a lista de arquivos e, se for um diretorio, registra o novo diretorio como chave
 	private static void createEntry(Stream<Path> pipeline) {
 		pipeline.forEach(path -> {
 			 if(Files.isDirectory(path)) {
@@ -59,24 +63,45 @@ public class ProcessQueuedFileEvents {
 		 });
 	}
 	
+	// remove um arquivo da lista de arquivos e, se for um diretorio, desregistra a chave do diretorio existente
 	private static void deleteEntry(Stream<Path> pipeline) {
 		pipeline.forEach(path -> {
 			if(Files.isDirectory(path)) {
 				System.out.println("Existing directory deleted. Deleting cache and reprocessing files...");
+				
+				try(Stream<Path> elements = Files.walk(path)) {
+					elements.filter(Files::isRegularFile)
+							.filter(Utils::isPropertiesFile)
+							.map(propertyFile -> {
+								Path jsonFilePath = Utils.resolveJsonFilePath(propertyFile);
+								try {
+									Files.deleteIfExists(jsonFilePath);
+								} catch (IOException e) {
+									e.printStackTrace();
+								}
+								return jsonFilePath;
+							})
+							.forEach(Values::deleteElementFromHashTableMap);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 				// deve verificar o cache e invalidar o diretorio excluido junto ao seu conteudo
 			} else {
 				// este objeto retornado deve ser usado para remover todas as entradas do .java e do .class
-				HashTableModel htm = Values.getElementFromHashTableMap(Utils.resolveJsonFilePath(path.getFileName()));
+				SyncSource syncSource = new SyncSource(path);
+				syncSource.eraseSection();
+				
 				/*
 				 * ...
 				 * ...
 				 */
 				// por fim, devemos deletar o arquivo de cache do sistema de arquivos e o cache do mapa de cache carregado
-				Values.deleteElementFromHashTableMap(path, htm);
+				Values.deleteElementFromHashTableMap(path);
 			}
 		});
 	}
 	
+	// modifica somente os dados dentro do arquivo. Toda operacao de modificacao opera em cima de alguma alteracao no conteudo do arquivo
 	private static void modifyEntry(Stream<Path> pipeline) {
 		pipeline.forEach(path -> {
 			if(Files.isDirectory(path)) {
