383f651fb03eaad49dc10d6e1d95d237d2505ff7
Deiv <32deiv@gmail.com>
2025-08-25 18:31:33 -0300

    changed the reference class to search for lib resources

--- src/main/java/com/dsl/classgen/io/file_manager/Compiler.java
+++ src/main/java/com/dsl/classgen/io/file_manager/Compiler.java
@@ -2,7 +2,6 @@ package com.dsl.classgen.io.file_manager;
 
 import javax.tools.ToolProvider;
 
-import com.dsl.classgen.annotation.processors.AnnotationProcessor;
 import com.dsl.classgen.io.StructureChecker;
 import com.dsl.classgen.io.SupportProvider;
 import com.dsl.classgen.utils.LogLevels;
@@ -18,7 +17,7 @@ public final class Compiler extends SupportProvider {
 
             int opStats = ToolProvider.getSystemJavaCompiler().run(null, null, null,
             		"-d", pathsCtx.getOutputClassFilePath().toString(),
-            		"--module-path", AnnotationProcessor.class.getResource("/libs").getPath(),
+            		"--module-path", Compiler.class.getResource("/libs").getPath(),
             		"-sourcepath", "/src/main/java/:" + pathsCtx.getOutputSourceDirPath().toString(),
             		pathsCtx.getExistingPJavaGeneratedSourcePath().toString());
             if(opStats == 0) {
